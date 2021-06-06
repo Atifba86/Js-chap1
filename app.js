ded7d1155f53a5eb5339fcda7ebf59d1333a7f1a
Atifba86 <70482315+Atifba86@users.noreply.github.com>
2021-06-06 17:42:46 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+// task 1
+//alert("Assalamu Alaikum")
+
+
+//task 2
+//alert("Error! Please Enter a Correct password.")
+
+//task 3
+// alert("Welcome to JS land...\rHappy Coding!")
+
+//task 4
+// alert("Welcome to JS land...")
+// alert("Happy Coding!\n prevent this page to add additional dialog boxes.")
+
+//task 5
+//console.log("Hello... I can run JS through my web brower's console" ),alert("Hello... I can run JS through my web brower's console")
